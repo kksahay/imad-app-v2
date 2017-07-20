82f4302ee616c41e8534567ce74dbc9f519a6cbb
kksahay <kunal.sahay4@gmail.com>
2017-07-20 12:45:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,61 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var articleOne ={
+  title: "Article one | Kunal Sahay",
+  heading: "Article one",
+  content:
+            `<p>
+                Contents
+            </p>
+            <p>
+                Contents
+            </p>
+            <p>
+                Contents
+            </p>
+            <p>
+                Contents
+            </p>`
+  
+};
+
+function createTemplate(data){
+    var title=data.title;
+    var heading=data.heading;
+    var content=data.content;
+
+var htmlTemplate=
+`<html>
+    <head>
+        <title>
+           ${title}
+        </title>
+        <link href="/ui/style.css" rel="stylesheet"/>
+        
+    </head>
+   
+    <body>
+        <div class="container">
+         <div>
+            <a href="/">Home</a>
+         </div>
+         <hr/>
+        <h3>
+            ${heading}
+        </h3>
+         <div>
+            ${content}
+         </div>
+        </div>
+    </body>
+</html>`
+;
+return htmlTemplate
+}
+
 app.get('/article-one',function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.send(createTemplate(articleOne));
 });
     
 app.get('/article-two',function(req,res){
