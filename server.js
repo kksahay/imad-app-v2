584ed62ad81d79618b791143b71bec551ea34e77
kksahay <kunal.sahay4@gmail.com>
2017-07-20 15:02:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ return htmlTemplate
 
 app.get('/:articleName',function(req,res){
     var articleName=req.params.articleName;
-   res.send(createTemplate(articles(articleName)));
+   res.send(createTemplate(articles[articleName]));
 });
     
 
